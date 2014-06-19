450621ac4c299f49004d2fafb71a005a6b587b39
Lance Batson <lancebatsondev@gmail.com>
2014-06-18 21:55:24 -0500

    added handlebars templates. show playlists and tracks

--- src/js/main.js
+++ src/js/main.js
@@ -1,22 +1,25 @@
 var auth_url      = 'https://accounts.spotify.com/authorize',
+    api_url       = 'https://api.spotify.com/v1',
     client_id     = 'f880219b69b349bf8683bbcd1091410a',
     redirect_uri  = 'http://localhost:8000',
     response_type = 'token',
     scopes        = 'playlist-read-private',
-    access_token  = '';
+    access_token  = '',
+    user          = {},
+    playlist      = {};
 
 $(function() {
   var url = window.location.href;
   if (url.search('#access_token') !== -1) {
+    $('#authorize').hide();
     var params = url.split('#')[1];
     params = $.getQueryParameters(params);
-    console.log(params);
     access_token = params.access_token;
-    retrieveUserInfo();
+    retrieveUserAndPlaylist();
   }
 });
 
-$('#test').on('click', function() {
+$('#authorize').on('click', function() {
   var encoded_url = auth_url + '?';
   encoded_url += $.param({
     client_id: client_id,
@@ -35,26 +38,46 @@ jQuery.extend({
   }
 });
 
-function retrieveUserInfo() {
+function retrieveUserAndPlaylist() {
   $.ajax({
-     url: 'https://api.spotify.com/v1/me',
-     headers: {
-         'Authorization': 'Bearer ' + access_token
-     },
-     success: function(response) {
-      retrievePlaylists(response.id);
-     }
+     url: api_url + '/me',
+     headers: { 'Authorization': 'Bearer ' + access_token }
+  })
+  .then(function(userInfo) {
+    user = userInfo;
+    return $.ajax({
+      url: api_url + '/users/' + user.id + '/playlists',
+      headers: { 'Authorization': 'Bearer ' + access_token }
+    });
+  })
+  .then(function(playlistInfo) {
+    playlist = playlistInfo;
+    console.log(playlistInfo);
+    displayPlaylistItems();
+  })
+  .fail(function(err) {
+    console.log(err);
   });
 }
 
-function retrievePlaylists(userID) {
-  $.ajax({
-     url: 'https://api.spotify.com/v1/users/' + userID + '/playlists',
-     headers: {
-         'Authorization': 'Bearer ' + access_token
-     },
-     success: function(response) {
-      console.log(response);
-     }
-  });
+function displayPlaylistItems() {
+  $('#results').empty();
+  var template = Handlebars.compile($("#playlist-list").html());
+  $('#results').append(template(playlist.items));
 }
+
+$(document).on('click', '.export', function() {
+  var playlist_id = $(this).data("id");
+  $.ajax({
+    url: api_url + '/users/' + user.id + '/playlists/'+ playlist_id + '/tracks',
+    headers: { 'Authorization': 'Bearer ' + access_token }
+  })
+  .then(function(result) {
+    $('#results').empty();
+    var template = Handlebars.compile($("#playlist-list-items").html());
+    $('#results').append(template(result.items));
+  })
+  .fail(function(err) {
+    console.log(err);
+  });
+});
