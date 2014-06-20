6e534911a9ce670de6abb62bd91b38d575741393
Lance Batson <lancebatsondev@gmail.com>
2014-06-20 11:06:09 -0500

    refactor app to use yeoman structure

---
+++ app/scripts/main.js
@@ -0,0 +1,85 @@
+console.log('\'Allo \'Allo!');
+
+var authUrl      = 'https://accounts.spotify.com/authorize',
+    apiUrl       = 'https://api.spotify.com/v1',
+    clientId     = 'f880219b69b349bf8683bbcd1091410a',
+    redirectUri  = 'http://localhost:8000',
+    responseType = 'token',
+    scopes        = 'playlist-read-private',
+    accessToken  = '',
+    user          = {},
+    playlist      = {};
+
+function displayPlaylistItems() {
+  $('#results').empty();
+  var template = Handlebars.compile($('#playlist-list').html());
+  $('#results').append(template(playlist.items));
+}
+
+function retrieveUserAndPlaylist() {
+  $.ajax({
+    url: apiUrl + '/me',
+    headers: { 'Authorization': 'Bearer ' + accessToken }
+  })
+  .then(function(userInfo) {
+    user = userInfo;
+    return $.ajax({
+      url: apiUrl + '/users/' + user.id + '/playlists',
+      headers: { 'Authorization': 'Bearer ' + accessToken }
+    });
+  })
+  .then(function(playlistInfo) {
+    playlist = playlistInfo;
+    console.log(playlistInfo);
+    displayPlaylistItems();
+  })
+  .fail(function(err) {
+    console.log(err);
+  });
+}
+
+jQuery.extend({
+  getQueryParameters: function(str) {
+    return (str || document.location.search).replace(/(^\?)/,'').split('&').map(
+      function(n){return n = n.split('='),this[n[0]] = n[1],this}.bind({})
+    )[0];
+  }
+});
+
+$('#authorize').on('click', function() {
+  var encodedUrl = authUrl + '?';
+  encodedUrl += $.param({
+    clientId: clientId,
+    redirectUri: redirectUri,
+    responseType: responseType,
+    scope: scopes
+  });
+  window.location.href = encodedUrl;
+});
+
+$(document).on('click', '.export', function() {
+  var playlistId = $(this).data('id');
+  $.ajax({
+    url: apiUrl + '/users/' + user.id + '/playlists/'+ playlistId + '/tracks',
+    headers: { 'Authorization': 'Bearer ' + accessToken }
+  })
+  .then(function(result) {
+    $('#results').empty();
+    var template = Handlebars.compile($('#playlist-list-items').html());
+    $('#results').append(template(result.items));
+  })
+  .fail(function(err) {
+    console.log(err);
+  });
+});
+
+$(function() {
+  var url = window.location.href;
+  if (url.search('#accessToken') !== -1) {
+    $('#authorize').hide();
+    var params = url.split('#')[1];
+    params = $.getQueryParameters(params);
+    accessToken = params.accessToken;
+    retrieveUserAndPlaylist();
+  }
+});
